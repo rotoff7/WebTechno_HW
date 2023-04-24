e3e25d5a4d2cbb48090258810bf53ea700ce7f2f
Maxim Gostry <rotoff7@mail.ru>
2023-04-24 21:09:33 +0300

    script V0.2

--- HomeWork2/scripts/script.js
+++ HomeWork2/scripts/script.js
@@ -1,3 +1,17 @@
+function isEmailValid(value) {
+ 	return EMAIL_REGEXP.test(value);
+}
+
+function onInput() {
+	if (isEmailValid(input.value)) {
+		input.style.borderColor = 'green';
+        input.style.backgroundColor = 'lightGreen';
+	} else {
+		input.style.borderColor = '#FF8A8AE8';
+        input.style.backgroundColor = '#FFFFFFE8';
+	}
+}
+
 function getMail(){
     var mail = document.getElementById("mailInput").value;
     if (document.getElementById("mailInput").value === ""){
@@ -6,19 +20,7 @@ function getMail(){
         alert(`Спасибо! Новости будут приходить по указанному адресу: ${mail}`);
     }
 }
-// const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-// const input = document.querySelector('input');
 
-// function isEmailValid(value) {
-//  	return EMAIL_REGEXP.test(value);
-// }
-
-// function onInput() {
-// 	if (isEmailValid(input.value)) {
-// 		input.style.borderColor = 'green';
-// 	} else {
-// 		input.style.borderColor = 'red';
-// 	}
-// }
-
-// input.addEventListener('input', onInput);
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+const input = document.querySelector('input');
+input.addEventListener('input', onInput);
